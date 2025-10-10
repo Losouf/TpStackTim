9f4c2e55c89003e46a94d1fe50ab41c78b4906c4
Livio OSOUF <149057423+Losouf@users.noreply.github.com>
2025-10-10 13:58:04 +0200

    Init XUnit Project

--- StackTimTPSoftware/StacktimApi.Tests/obj/Debug/net8.0/StacktimApi.Tests.AssemblyInfo.cs
+++ StackTimTPSoftware/StacktimApi.Tests/obj/Debug/net8.0/StacktimApi.Tests.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StackTimAPI")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("StacktimApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d53d9b025e45e6d828fc02ba4da83be671f69bc")]
-[assembly: System.Reflection.AssemblyProductAttribute("StackTimAPI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("StackTimAPI")]
+[assembly: System.Reflection.AssemblyProductAttribute("StacktimApi.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("StacktimApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
